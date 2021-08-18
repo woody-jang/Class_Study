62611c1e939f4579c06233fe9d9c61611079e7bb
장세호(Jang Seho) <82800087+woody-jang@users.noreply.github.com>
2021-08-18 17:27:07 +0900

    Add files via upload

--- Main2.java
+++ Main2.java
@@ -1,100 +1,83 @@
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+class Buffer {
+	private int data;
+	private boolean empty = true;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
+	public synchronized int get() {
+		while (empty) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		empty = true;
+		notifyAll();
+		return data;
+	}
 
-class MyDB implements Runnable {
-	public MyDB() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+	public synchronized void put(int data) {
+		while (!empty) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
 		}
+		empty = false;
+		this.data = data;
+		notifyAll();
 	}
-	private void insertInt(int i) {
-		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "root");
-				PreparedStatement stmt = conn.prepareStatement("INSERT INTO test (input) VALUES (?)");){
-			stmt.setInt(1, i);
-			
-			stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+}
+
+class Producer implements Runnable {
+	private Buffer buffer;
+
+	public Producer(Buffer buffer) {
+		this.buffer = buffer;
 	}
+
 	@Override
 	public void run() {
-		for (int i = 200; i <= 300; i++) {
-			insertInt(i);
+		for (int i = 0; i < 10; i++) {
+			buffer.put(i);
+			System.out.println("생산자: " + i + "번째 치킨을 생산");
+
 			try {
-				Thread.sleep(1000);
+				Thread.sleep((int) (Math.random() * 2000));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("DB 작업 종료");
 	}
 }
 
-class MyFrame extends JFrame {
-	private JLabel lblNum;
-	private int number;
-	private Timer timer;
-	
-	public MyFrame() {
-		number = 100;
-		lblNum = new JLabel(String.valueOf(number));
-		lblNum.setFont(new Font("굴림", Font.PLAIN, 30));
-		
-		add(lblNum);
-		
-		timer = new Timer(1000, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				number++;
-				lblNum.setText(String.valueOf(number));
-				
-				if (number == 200) {
-					System.out.println("스윙 작업 종료");
-					timer.stop();
-				}
-			}
-		});
-		timer.start();
-		
-		setSize(500, 500);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
+class Consumer implements Runnable {
+	private Buffer buffer;
+
+	public Consumer(Buffer buffer) {
+		this.buffer = buffer;
 	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			int data = buffer.get();
+			System.out.println("소비자: " + data + "번 치킨을 소비");
+
+			try {
+				Thread.sleep((int) (Math.random() * 2000));
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
 }
 
 public class Main2 {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new MyFrame();
-			}
-		});
-		
-		Thread dbThread = new Thread(new MyDB());
-		dbThread.start();
-		
-		for (int i = 0; i <= 100; i++) {
-			System.out.println(i);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("콘솔 출력 완료");
+		Buffer buffer = new Buffer();
+		Thread prod = new Thread(new Producer(buffer));
+		Thread cons = new Thread(new Consumer(buffer));
+
+		prod.start();
+		cons.start();
 	}
 }
